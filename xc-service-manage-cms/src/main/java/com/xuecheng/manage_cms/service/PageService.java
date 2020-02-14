45cdcaad7e2b1ba17ec0e8f9b40e5fd4b4189090
Howay <heweihzl@163.com>
2020-02-14 23:06:09 +0800

    删除页面服务端开发服务端完成

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -6,6 +6,7 @@ import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -128,6 +129,16 @@ public class PageService {
        }
         //返回失败
         return new CmsPageResult(CommonCode.FAIL,null);
+    }
+
+    //删除页面
+    public ResponseResult deleteCmspage(String id){
+        CmsPage cmsPage = findById(id);
+        if (cmsPage!=null){
+            cmsPageRepository.delete(cmsPage);
+            return new ResponseResult(CommonCode.SUCCESS);
+        }
+        return new ResponseResult(CommonCode.FAIL);
     }
 
 }
