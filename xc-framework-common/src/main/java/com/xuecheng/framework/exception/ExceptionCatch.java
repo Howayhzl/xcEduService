d4bfdb4013d3950b21fbfd316d68dc19e7ec7d45
Howay <heweihzl@163.com>
2020-02-15 11:35:30 +0800

    不可预知异常处理完成

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -1,9 +1,12 @@
 package com.xuecheng.framework.exception;
 
+import com.google.common.collect.ImmutableMap;
+import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.framework.model.response.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +17,10 @@ public class ExceptionCatch {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionCatch.class);
 
+    //定义map，配置异常类所对应的的错误代码
+     private static ImmutableMap<Class<? extends  Throwable>,ResultCode> EXCEPTIONS;
+    //定义map的builder对象,去构建ImmutableMap
+    protected static ImmutableMap.Builder<Class<? extends  Throwable>,ResultCode> builder = ImmutableMap.builder();
 
     @ExceptionHandler(CustomException.class)
     @ResponseBody
@@ -22,5 +29,27 @@ public class ExceptionCatch {
         ResultCode resultCode = e.getResultCode();
         ResponseResult responseResult = new ResponseResult(resultCode);
         return responseResult;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseResult exception(Exception e){
+        //记录日志
+        LOGGER.error("catch exception:{}",e.getMessage());
+        if (EXCEPTIONS==null){
+            EXCEPTIONS = builder.build(); //EXCEPTIONS构建成功
+        }
+        //从EXCEPTIONS中找异常类型所对应的错误代码,如果找到了将错误代码响应给用户，如果找不到给用户响应9999异常
+        ResultCode resultCode = EXCEPTIONS.get(e.getClass());
+        if (resultCode!=null){
+            return new ResponseResult(resultCode);
+        }else {
+            return new ResponseResult(CommonCode.SERVER_ERROR);
+        }
+    }
+
+    static {
+        //定义异常类型所对应的错误代码
+        builder.put(HttpMessageNotReadableException.class,CommonCode.INVALID_PARAM);
     }
 }
