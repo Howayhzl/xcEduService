b6e7585190a3ad876b4cd1334cded1b34d16987e
Howay <heweihzl@163.com>
2020-02-17 16:55:19 +0800

    RabbitMQ入门程序生产者编写完成，测试OK

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -10,11 +10,10 @@ import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EntityScan("com.xuecheng.framework.domain.cms") // 扫描实体类
-@ComponentScan("com.xuecheng.api.cms") //扫描接口
-@ComponentScan("com.xuecheng.api.cms") //扫描接口
-@ComponentScan("com.xuecheng.framework") // 扫描common类
-@ComponentScan(basePackages = "com.xuecheng.manage_cms")//扫描本项目下所有的类
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
+@ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common包下的类
+@ComponentScan(basePackages={"com.xuecheng.manage_cms"})//扫描本项目下的所有类
 public class ManageCmsApplication {
 
     public static void main(String[] args) {
