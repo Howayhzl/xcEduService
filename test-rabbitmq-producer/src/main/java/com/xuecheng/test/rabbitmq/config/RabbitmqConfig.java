8170f8009339239b888a068456f7c80269b01dd1
Howay <heweihzl@163.com>
2020-02-22 21:01:06 +0800

    Spring 整合RibbitMQ 搭建SpringBoot环境完成

---
+++ test-rabbitmq-producer/src/main/java/com/xuecheng/test/rabbitmq/config/RabbitmqConfig.java
@@ -0,0 +1,14 @@
+package com.xuecheng.test.rabbitmq.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    //声明交换机
+
+    //声明队列
+
+    //绑定交换机和队列
+}
