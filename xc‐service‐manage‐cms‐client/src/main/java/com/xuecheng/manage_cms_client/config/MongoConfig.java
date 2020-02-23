92ba0fe7ce9ff5864f1cd29f3ca2991f8322e114
Howay <heweihzl@163.com>
2020-02-23 22:34:28 +0800

    页面发布消费方Cms Client的service方法完成

---
+++ xc‐service‐manage‐cms‐client/src/main/java/com/xuecheng/manage_cms_client/config/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.xuecheng.manage_cms_client.config;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.database}")
+    String db;
+
+    @Bean
+    public GridFSBucket getGridFSBucket(MongoClient mongoClient){
+        MongoDatabase database = mongoClient.getDatabase(db);
+        GridFSBucket gridFSBucket = GridFSBuckets.create(database);
+        return gridFSBucket;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    }
+}
---
+++ xc‐service‐manage‐cms‐client/src/main/java/com/xuecheng/manage_cms_client/service/PageService.java
@@ -0,0 +1,126 @@
+package com.xuecheng.manage_cms_client.service;
+
+
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.CmsSite;
+import com.xuecheng.manage_cms_client.dao.CmsPageRepository;
+import com.xuecheng.manage_cms_client.dao.CmsSiteRepository;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.util.Optional;
+
+@Service
+public class PageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PageService.class);
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+    @Autowired
+    CmsSiteRepository cmsSiteRepository;
+
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Autowired
+   GridFSBucket gridFSBucket;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //将页面html保存到页面物理路径
+    public void savePageToServerPath(String pageId){
+        //根据pageId获取CmsPage
+        CmsPage cmsPage = this.findCmsPageByPageId(pageId);
+        //获取html的fileId，从CmsPage中获取
+        String htmlFileId = cmsPage.getHtmlFileId();
+        //从gridFS中查询html,根据htmlFileId从GridDF中查询文件内容
+        InputStream inputStream = this.getFileById(htmlFileId);
+        if (inputStream==null){
+            logger.error("getFileById InputStream is null,htmlFileId:{}",htmlFileId);
+            return;
+        }
+        //将页面html保存到页面物理路径
+
+        // 获取站点信息
+        CmsSite cmsSite = this.findCmsSiteByPageId(cmsPage.getSiteId());
+        //获取站点物理路径
+        String sitePhysicalPath = cmsSite.getSitePhysicalPath();
+        //获取页面的物理路径
+        String pagePath = sitePhysicalPath + cmsPage.getPagePhysicalPath() + cmsPage.getPageName();
+
+        //将html文件保存到服务器的物理路径上
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(new File(pagePath));
+            IOUtils.copy(inputStream,fileOutputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    //根据pageId获取CmsPage
+    private CmsPage  findCmsPageByPageId(String siteId){
+        Optional<CmsPage> optional = cmsPageRepository.findById(siteId);
+        if (optional.isPresent()){
+            CmsPage cmsPage = optional.get();
+            return cmsPage;
+        }
+        return null;
+    }
+
+
+    //根据pageId获取CmsSite
+    private CmsSite findCmsSiteByPageId(String pageId){
+        Optional<CmsSite> optional = cmsSiteRepository.findById(pageId);
+        if (optional.isPresent()){
+            CmsSite cmsSite = optional.get();
+            return cmsSite;
+        }
+        return null;
+    }
+
+    //根据htmlFileId从GridDF中查询文件内容
+    private InputStream getFileById(String htmlFileId){
+        //根据htmlFileId获取文件对象
+        GridFSFile gridFSFile = gridFsTemplate.findOne(Query.query(Criteria.where("_id").is(htmlFileId)));
+        //打开下载流
+        GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(gridFSFile.getObjectId());
+        //定义GridFSResource
+        GridFsResource gridFsResource = new GridFsResource(gridFSFile,gridFSDownloadStream);
+        try {
+            InputStream inputStream = gridFsResource.getInputStream();
+            return inputStream;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
