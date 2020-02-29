fdec03ade500bde1791778e508668dd5efca2d7d
Howay <heweihzl@163.com>
2020-03-01 00:31:27 +0800

    定义课程计划查询接口

---
+++ xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
@@ -0,0 +1,14 @@
+package com.xuecheng.api.course;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value="课程管理接口",description = "课程管理接口，提供课程的增、删、改、查")
+public interface CourseControllerApi {
+
+    @ApiOperation("课程计划查询")
+    public TeachplanNode findTeachplanList(String courseId);
+}
