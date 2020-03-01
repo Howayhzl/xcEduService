d216b93acbe2ea40375f90015523e6a95826caf6
Howay <heweihzl@163.com>
2020-03-01 12:57:03 +0800

    PageHelper测试完成

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/TeachplanRepository.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/TeachplanRepository.java
@@ -9,4 +9,6 @@ public interface TeachplanRepository extends JpaRepository<Teachplan,String> {
 
     //定义方法根据课程id和父结点id查询出结点列表，可以使用此方法实现查询根结点
     public List<Teachplan> findByCourseidAndParentid(String courseId, String parentId);
+
+    
 }
